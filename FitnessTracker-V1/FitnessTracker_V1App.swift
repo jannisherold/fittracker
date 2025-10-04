9291fd97a25cb443b8b7c21c0bdccabe2b3b207b
jannisherold <130995436+jannisherold@users.noreply.github.com>
2025-10-04 12:48:21 +0200

    Revert "trying to setup firebase"
    This reverts commit 7558ffc637302a24d9b532522d98905f9d9e2db5.

--- FitnessTracker-V1/FitnessTracker_V1App.swift
+++ FitnessTracker-V1/FitnessTracker_V1App.swift
@@ -1,29 +1,7 @@
 import SwiftUI
 
-import FirebaseCore
-
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-
-  func application(_ application: UIApplication,
-
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-
-    FirebaseApp.configure()
-
-    return true
-
-  }
-
-}
-
-
 @main
 struct FitnessTrackerV1App: App {
-    // register app delegate for Firebase setup
-
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
     @StateObject private var store = Store()
 
     var body: some Scene {
